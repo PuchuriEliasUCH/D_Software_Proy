b7dac91aa5960ae0b89a6cd67e9c8cb76f0e1884
PuchuriEliasUCH <elipuchuries@uch.pe>
2025-08-28 22:53:59 -0500

    clase Reserva

---
+++ src/main/java/com/s_reservas/sreservas/models/Reservas.java
@@ -0,0 +1,17 @@
+package com.s_reservas.sreservas.models;
+
+public class Reservas {
+    private int id_reserva;
+
+    public Reservas(int id_reserva) {
+        this.id_reserva = id_reserva;
+    }
+
+    public int getId_reserva() {
+        return id_reserva;
+    }
+
+    public void setId_reserva(int id_reserva) {
+        this.id_reserva = id_reserva;
+    }
+}
