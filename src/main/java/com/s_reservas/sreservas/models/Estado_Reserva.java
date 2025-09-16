63c0b03d0992b88d4660cbb1020d5e6db0c9cad8
Jano <janot.carranza@gmail.com>
2025-09-15 23:24:02 -0500

    Primera actualización

--- src/main/java/com/s_reservas/sreservas/models/Estado_Reserva.java
+++ src/main/java/com/s_reservas/sreservas/models/Estado_Reserva.java
@@ -1,4 +1,43 @@
 package com.s_reservas.sreservas.models;
 
 public class Estado_Reserva {
+    private String nombre;
+    private String descripcion;
+    private String color;
+    private boolean activo;
+
+    public Estado_Reserva () {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
 }
--- src/main/java/com/s_reservas/sreservas/models/Usuario.java
+++ src/main/java/com/s_reservas/sreservas/models/Usuario.java
@@ -1,4 +1,61 @@
 package com.s_reservas.sreservas.models;
 
 public class Usuario {
+    private String nombre;
+    private String apellido;
+    private String dni;
+    private String telefono;
+    private String contrasena;
+    private boolean activo;
+
+    public Usuario() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
 }
